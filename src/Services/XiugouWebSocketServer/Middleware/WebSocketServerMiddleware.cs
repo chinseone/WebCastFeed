874de3923e1e29e29d5660452fbf75f0bda8b7a5
Li Cheng <lcheng@roblox.com>
2022-04-21 22:18:48 +0800

    handle json and close

--- src/Services/XiugouWebSocketServer/Middleware/WebSocketServerMiddleware.cs
+++ src/Services/XiugouWebSocketServer/Middleware/WebSocketServerMiddleware.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
 
 namespace XiugouWebSocketServer.Middleware
 {
@@ -27,17 +29,38 @@ namespace XiugouWebSocketServer.Middleware
             {
                 var webSocket = await context.WebSockets.AcceptWebSocketAsync();
                 Console.WriteLine("WebSocket Connected");
+
                 var connId = _Manager.AddSocket(webSocket);
+                await SendConnIdAsync(webSocket, connId);
 
                 await ReceiveMessage(webSocket, async (result, buffer) =>
                 {
                     if (result.MessageType == WebSocketMessageType.Text)
                     {
+                        var message = Encoding.UTF8.GetString(buffer, 0, result.Count);
                         Console.WriteLine("Message received");
-                        Console.WriteLine($"Message {Encoding.UTF8.GetString(buffer, 0, result.Count)}");
+                        Console.WriteLine($"Message {message}");
+                        await RouteJsonMessage(message);
                     }
                     else if (result.MessageType == WebSocketMessageType.Close)
                     {
+                        var id = _Manager.GetAllSockets()
+                            .FirstOrDefault(s => s.Value == webSocket).Key;
+
+                        _Manager.GetAllSockets().TryRemove(id, out WebSocket sock);
+
+                        if (sock?.State == WebSocketState.Open)
+                        {
+                            var closeStatus = result.CloseStatus;
+                            if (closeStatus != null)
+                            {
+                                await sock.CloseAsync(
+                                    closeStatus.Value,
+                                    result.CloseStatusDescription,
+                                    CancellationToken.None);
+                            }
+                        }
+                        
                         Console.WriteLine("Received closed message");
                     }
                 });
@@ -47,6 +70,12 @@ namespace XiugouWebSocketServer.Middleware
                 Console.WriteLine("Hello from the 2nd request delegate");
                 await _Next(context);
             }
+        }
+
+        private async Task SendConnIdAsync(WebSocket socket, string connId)
+        {
+            var buffer = Encoding.UTF8.GetBytes($"ConnID: {connId}");
+            await socket.SendAsync(buffer, WebSocketMessageType.Text, true, CancellationToken.None);
         }
 
         private async Task ReceiveMessage(WebSocket webSocket, Action<WebSocketReceiveResult, byte[]> handleMessage)
@@ -59,6 +88,37 @@ namespace XiugouWebSocketServer.Middleware
                     cancellationToken: CancellationToken.None);
 
                 handleMessage(result, buffer);
+            }
+        }
+
+        public async Task RouteJsonMessage(string message)
+        {
+            try
+            {
+                var routeObj = JsonConvert.DeserializeObject<dynamic>(message);
+                if (Guid.TryParse(routeObj.To.ToString(), out Guid guidOutput))
+                {
+                    Console.WriteLine("Guid Parsed successfully");
+                }
+                else
+                {
+                    Console.WriteLine("No Guid, continue");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Input may not necessary be a JSON");
+            }
+
+
+            Console.WriteLine("Broadcast");
+            foreach (var socket in _Manager.GetAllSockets())
+            {
+                if (socket.Value.State == WebSocketState.Open)
+                {
+                    await socket.Value.SendAsync(Encoding.UTF8.GetBytes(message),
+                        WebSocketMessageType.Text, true, CancellationToken.None);
+                }
             }
         }
     }
