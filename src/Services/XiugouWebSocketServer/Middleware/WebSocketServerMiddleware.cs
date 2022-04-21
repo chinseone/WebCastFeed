04e65e70aaaaefd57f7adf8aa0e94e30a8ae3531
Li Cheng <lcheng@roblox.com>
2022-04-21 17:26:31 +0800

    web socket server with middleware

---
+++ src/Services/XiugouWebSocketServer/Middleware/WebSocketServerMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace XiugouWebSocketServer.Middleware
+{
+    public class WebSocketServerMiddleware
+    {
+        private readonly RequestDelegate _Next;
+
+        public WebSocketServerMiddleware(RequestDelegate next)
+        {
+            _Next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            if (context.WebSockets.IsWebSocketRequest)
+            {
+                var webSocket = await context.WebSockets.AcceptWebSocketAsync();
+                Console.WriteLine("WebSocket Connected");
+
+                await ReceiveMessage(webSocket, async (result, buffer) =>
+                {
+                    if (result.MessageType == WebSocketMessageType.Text)
+                    {
+                        Console.WriteLine("Message received");
+                        Console.WriteLine($"Message {Encoding.UTF8.GetString(buffer, 0, result.Count)}");
+                    }
+                    else if (result.MessageType == WebSocketMessageType.Close)
+                    {
+                        Console.WriteLine("Received closed message");
+                    }
+                });
+            }
+            else
+            {
+                Console.WriteLine("Hello from the 2nd request delegate");
+                await _Next(context);
+            }
+        }
+
+        private async Task ReceiveMessage(WebSocket webSocket, Action<WebSocketReceiveResult, byte[]> handleMessage)
+        {
+            var buffer = new byte[1024 * 4];
+
+            while (webSocket.State == WebSocketState.Open)
+            {
+                var result = await webSocket.ReceiveAsync(buffer: new ArraySegment<byte>(buffer),
+                    cancellationToken: CancellationToken.None);
+
+                handleMessage(result, buffer);
+            }
+        }
+    }
+}
---
+++ src/Services/XiugouWebSocketServer/Startup.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using XiugouWebSocketServer.Middleware;
+
+namespace XiugouWebSocketServer
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseWebSockets();
+
+            app.UseWebSocketServer();
+
+            app.Run(async context =>
+            {
+                Console.WriteLine("Hello from the 3rd request delegate");
+                await context.Response.WriteAsync("Hello from the 3rd request delegate");
+            });
+        }
+    }
+}
