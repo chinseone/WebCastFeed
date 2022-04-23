e8e28d3072aeb4702ebcfa616d93547ce65f2728
Li Cheng <chinseone@gmail.com>
2022-04-23 23:23:10 +0800

    respect tc mysql (#15)

--- src/SQLScripts/Tables/Users.sql
+++ src/SQLScripts/Tables/Users.sql
@@ -1,23 +1,15 @@
-USE xiugou_local;
-
-CREATE TABLE IF NOT EXISTS Users (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT,
-    userId VARCHAR(256) NOT NULL,
-    platform INT NOT NULL,
-    nickName VARCHAR(256),
-    ticketId INT,
-    messageCount INT NOT NULL,
-    totalPay INT NOT NULL,
-    totalPayGuest INT NOT NULL,
-    joinTimestamp DATETIME NOT NULL,
-    LastTimestamp DATETIME NOT NULL,
-    createdUtc DATETIME NOT NULL,
-    updatedUtc DATETIME NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=COMPRESSED;
-
-# INDEX userIdPlatform(userId, platform)
-SET @x := (SELECT COUNT(*) FROM INFORMATION_SCHEMA.STATISTICS WHERE TABLE_NAME = 'Users' AND INDEX_NAME = 'userIdPlatform' AND TABLE_SCHEMA = DATABASE());
-SET @y := 'Alter Table Users ADD UNIQUE Index userIdPlatform(userId, platform);';
-SET @sql := if( @x > 0, 'select ''Index code exists.''', @y);
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
+CREATE TABLE `Users` (
+  `id` BIGINT PRIMARY KEY AUTO_INCREMENT,
+  `userId` varchar (256) NOT NULL DEFAULT '',
+  `platform` int NOT NULL,
+  `nickName` varchar (256) NULL DEFAULT '',
+  `ticketId` int NULL,
+  `messageCount` int NOT NULL DEFAULT 0,
+  `totalPay` int NOT NULL DEFAULT 0,
+  `totalPayGuest` int NOT NULL DEFAULT 0,
+  `joinTimestamp` datetime NULL,
+  `lastTimestamp` datetime NULL,
+  `createdUtc` datetime NULL,
+  `updatedUtc` datetime NULL,
+  unique `userIdPlatform` USING btree (`userId`, `platform`)
+) ENGINE = innodb DEFAULT CHARACTER SET = "utf8mb4" COLLATE = "utf8mb4_0900_ai_ci" ROW_FORMAT = Compressed
