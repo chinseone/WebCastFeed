58c61c6a79bf022f9a6cf78cb1199845d9ed910a
Li Cheng <chinseone@gmail.com>
2022-04-24 21:56:35 +0800

    change webhook validation response (#18)

---
+++ src/Services/WebCastFeed/Models/Response/DouyinWebhookValidateResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WebCastFeed.Models.Response
+{
+    public class DouyinWebhookValidateResponse
+    {
+        [JsonPropertyName("challenge")]
+        public int Challenge { get; set; }
+    }
+}
