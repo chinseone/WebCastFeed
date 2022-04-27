0e196b3143c5740fda01cd525c0846b743197b9f
Li Cheng <lcheng@roblox.com>
2022-04-27 16:23:59 +0800

    debug

--- src/Services/WebCastFeed/Models/DouyinMessage.cs
+++ src/Services/WebCastFeed/Models/DouyinMessage.cs
@@ -4,13 +4,7 @@ namespace WebCastFeed.Models.Requests
 {
     public class DouyinMessage
     {
-        [JsonPropertyName("msg_type")]
+        [JsonPropertyName("A")]
         public string MessageType { get; set; }
-
-        [JsonPropertyName("session_id")]
-        public string SessionId { get; set; }
-
-        [JsonPropertyName("payload")]
-        public DouyinPayload Payload { get; set; }
     }
 }
