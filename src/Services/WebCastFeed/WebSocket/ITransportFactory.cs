d174754243d25b0624b257e807f60ecadc097584
Li Cheng <chinseone@gmail.com>
2022-05-07 00:09:32 +0800

    Keep connected (#42)
    * so far
    
    * seems to work

---
+++ src/Services/WebCastFeed/WebSocket/ITransportFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebCastFeed.WebSocket
+{
+    public interface ITransportFactory
+    {
+        Task<IAsyncTransport> CreateTransportAsync(CancellationToken cancellationToken);
+    }
+}
