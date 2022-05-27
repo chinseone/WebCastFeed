2f7c28ab022f18930fe60358297e7fd37a9ab67c
Li Cheng <chinseone@gmail.com>
2022-05-27 16:07:20 +0800

    chat controller (#56)

---
+++ src/Services/WebCastFeed/Controllers/ChatController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebCastFeed.Models.Requests;
+
+namespace WebCastFeed.Controllers
+{
+    [Route("v1/blue-dash")]
+    [ApiController]
+    public class ChatController : Controller
+    {
+        [HttpPost("messages")]
+        [Consumes("application/json")]
+        public IActionResult AcceptMessages(
+            [FromBody]MessageBody message)
+        {
+            return Ok();
+        }
+    }
+}
